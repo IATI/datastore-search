c25e1520133fad1e5320d913d11d498a9ec257fa
Alex Lydiate <alexl@devinit.org>
2021-12-16 12:40:57 +0000

    Now With Added Vite

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <div id="app">
-    <NavBar />
-    <router-view />
-  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
-<script type="text/javascript">
-import NavBar from "@/components/NavBar.vue";
-export default {
-  components: {
-    NavBar, // register component
-  },
-};
-</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
