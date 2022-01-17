8c3cbb12c944cf0d78db4353f81a94321d146044
Alex Lydiate <alexl@devinit.org>
2022-01-17 12:38:51 +0000

    Added operator for Select filters

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -21,11 +21,38 @@
         <!-- Text inputs -->
         <input class="h-10 float-left border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" v-if="global.fieldType(filter.field) === 'text'" placeholder="Solr search term" v-on:change="global.changeFilter(filter.id, 'value', $event.target.value)">
         <!-- Select inputs -->
-        <select class="h-10 float-left bg-white border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" v-if="global.fieldType(filter.field) === 'select'" @change="global.changeFilter(filter.id, 'value', $event.target.value)">
-          <option>Fake One</option>
-          <option>Fake Two</option>
-          <option>Fake Three</option>
-        </select>
+        <div class="grid grid-cols-8 gap-2" v-if="global.fieldType(filter.field) === 'select'">
+          <div class="col-span-3">
+            <div class="flex items-center justify-center">
+              <div class="inline-flex" role="toolbar">
+                <button
+                  :class="{'bg-blue-300': (filter.operator === 'equals')}"
+                  @click="global.changeFilter(filter.id, 'operator', 'equals')"
+                  type="button" 
+                  class="h-10 border-l border-t border-b rounded-l px-2 py-2 text-gray-700 font-medium text-xs leading-tight uppercase hover:bg-blue-500 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out"
+                >
+                  ==
+                </button>
+                <button
+                  :class="{'bg-blue-300': (filter.operator === 'notEquals')}"
+                  @click="global.changeFilter(filter.id, 'operator', 'notEquals')"
+                  type="button" 
+                  class="h-10 border-r border-t border-b rounded-r px-2 py-2 text-gray-700 font-medium text-xs leading-tight uppercase hover:bg-blue-500 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out"
+                >
+                  !=
+                </button>
+              </div>
+            </div>
+          </div>
+          <div class="col-span-5">
+            <select class="h-10 float-left bg-white border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline" v-if="global.fieldType(filter.field) === 'select'" @change="global.changeFilter(filter.id, 'value', $event.target.value)">
+              <option>Fake One</option>
+              <option>Fake Two</option>
+              <option>Fake Three</option>
+            </select>
+          </div>
+        </div>
+
         <!-- Date inputs -->
         <div class="grid grid-cols-8 gap-2" v-if="global.fieldType(filter.field) === 'date'">
           <div class="col-span-3">
