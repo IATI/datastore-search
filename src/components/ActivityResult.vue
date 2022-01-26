c5370464c88a2c8f33a2114ad22626d03b1abdc4
Alex Lydiate <alexl@devinit.org>
2022-01-26 18:03:56 +0000

    trying to fix a problem apparent on dev but not locally #6ffs

--- src/components/ActivityResult.vue
+++ src/components/ActivityResult.vue
@@ -12,7 +12,7 @@
 <template>
   <div class="flex flex-col h-full">
     <div class="flex-grow"><router-view />
-        <div v-if="typeof this.activity !== undefinged && this.activity != null" class="grid grid-cols-10 gap-4 text-left mb-5">
+        <div class="grid grid-cols-10 gap-4 text-left mb-5">
           <div class="col-span-1"></div>
               <div class="col-span-8 mt-10 text-2xl border-b pb-3">{{ activity.title_narrative[0] }}</div>
           <div class="col-span-1"></div>
