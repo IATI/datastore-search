a0041455266c379c11428a093a9ab41689d77eae
nosvalds <niko@devinit.org>
2022-01-26 14:22:20 +0000

    download buttons working on simple results page for smaller queries

---
+++ src/components/DownloadButtons.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="result-download-buttons" class="pt-5 flex mx-5">    
+        <button  v-for="option in global.state.downloadOptions" :key="option.format" v-on:click="global.downloadFile(option.format)" class="bg-iati-blue hover:bg-iati-grey text-white font-bold py-1 px-1 rounded flex ml-8 w-2/24 pr-2">
+            <ArrowDownIcon class="h-5 w-5 text-grey-300 mr-1"/>
+            <span v-if="!option.isLoading">{{ option.format }}</span>
+            <div>
+                <div v-if="option.isLoading" style="border-top-color:transparent"
+                    class="w-6 h-6 p-2 border-4 border-white border-dotted rounded-full animate-spin">
+                </div>
+            </div>
+        </button>
+    </div> 
+</template>
+
+<script>
+import { ArrowDownIcon } from '@heroicons/vue/solid'
+
+export default {
+    name: 'DownloadButtons',
+    inject: ["global"],
+    components: {ArrowDownIcon}
+}
+</script>
+
+<style>
+
+</style>
--- src/global.js
+++ src/global.js
@@ -11,6 +11,7 @@ const axiosConfig = {
 const baseUrl = "https://dev-api.iatistandard.org/dss/activity/select?wt=json&sort=iati_identifier asc&fl=title_narrative,description_narrative,iati_identifier,last_updated_datetime,reporting_org_narrative&rows=10&hl=true&hl.method=unified&hl.fl=*_narrative&q=";
 const baseUrlSimple = "https://dev-api.iatistandard.org/dss/activity/search?wt=json&sort=iati_identifier asc&fl=title_narrative,description_narrative,iati_identifier,last_updated_datetime,reporting_org_narrative&rows=10&hl=true&hl.method=unified&hl.fl=*_narrative&q="
 const baseUrlActivity = "https://dev-api.iatistandard.org/dss/activity/select?wt=json&sort=iati_identifier asc&fl=title_narrative,description_narrative,iati_identifier,last_updated_datetime,reporting_org_narrative&rows=1&hl=true&hl.method=unified&hl.fl=*_narrative&q=";
+const baseUrlDownload = "https://dev-api.iatistandard.org/dss/download"
 
 const state = reactive({
   nextFilterId: 0,
@@ -29,7 +30,8 @@ const state = reactive({
   resultsPerPage: 10, //results shown per page of UI
   page: null,
   simpleSearch: null,
-  activity: null
+  activity: null,
+  downloadOptions: [{ format: 'XML', isLoading: false },{ format: 'JSON', isLoading: false },{ format: 'CSV', isLoading: false }]
 });
 
 //API implementation, and then exported:
@@ -148,16 +150,32 @@ const loadActivity = async (iatiIdentifier) => {
   state.activity = result.data.response.docs[0];
 }
 
-const downloadXML = () => {
-  alert('Yet to be implemented');
+const isFileLoading = (format) => {
+  return state.downloadOptions.find(val => val.format === format).isLoading
 }
 
-const downloadJSON = () => {
-  alert('Yet to be implemented');
+const downloadFile = async (format) => {
+  try {
+    state.downloadOptions.find(val => val.format === format).isLoading = true
+    const response = await axios.post(baseUrlDownload, {
+      query: `activity/search?sort=iati_identifier asc&q=${state.query}`,
+      format,
+    }, axiosConfig);
+    await downloadItem({ url: response.data.url, label: response.data.fileName})
+    state.downloadOptions.find(val => val.format === format).isLoading = false
+  } catch (error) {
+    console.error(error)
+    alert(`Download Failed: ${error.message}`)
+    state.downloadOptions.find(val => val.format === format).isLoading = false
+  }
 }
 
-const downloadCSV = () => {
-  alert('Yet to be implemented');
+const downloadItem= async ({ url, label }) => {
+  const link = document.createElement("a");
+  link.download = label; 
+  link.href = new URL(url);
+  link.click();
+  URL.revokeObjectURL(link.href);
 }
 
 const paginationUpdate = (page) => {
@@ -228,8 +246,7 @@ export default { state: readonly(state),
   exportFilters,
   run,
   runSimple,
-  downloadCSV,
-  downloadJSON,
-  downloadXML,
+  isFileLoading,
+  downloadFile,
   paginationUpdate
   };
