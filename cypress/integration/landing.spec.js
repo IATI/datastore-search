a8f9217d0291d0f1d48fd0cbc7a763f26d9dcbb7
akmiller01 <alex.k.miller@gmail.com>
2022-02-04 14:39:35 -0500

    Simple spec successfully failing

--- cypress/integration/landing.spec.js
+++ cypress/integration/landing.spec.js
@@ -1,10 +1,17 @@
 /// <reference types="Cypress"/>
 
-describe('The landing page', () => {
+describe('The landing view', () => {
 
   it('has a header', () => {
     cy.visit('/');
     cy.contains('Datastore Search');
+  });
+
+  it('has a visible search bar', () => {
+    cy.visit('/');
+    cy
+      .get('input[title=Search]')
+      .should('be.visible');
   });
 
   it('has a footer', () => {
@@ -13,11 +20,17 @@ describe('The landing page', () => {
   });
 
   describe('search bar', () => {
-    it('successfully routes a search to the simple page', () => {
+
+    it('routes a search to the simple view', () => {
       cy.get('input[title=Search]').type('test');
       cy.get('button[aria-label=Submit]').click();
       cy.url().should('includes', '/simple');
     });
+
+    it('retains the search term after routing', () => {
+      cy.get('input[title=Search]').should('have.value', 'test')
+    });
+
   });
 
 });
