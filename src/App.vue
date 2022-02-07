a5844b96475eb3c7ec7bf85d06018d594967326f
akmiller01 <alex.k.miller@gmail.com>
2022-02-07 12:10:39 -0500

    Margin top to avoid text wrap on certain widths

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       <div class="bg-iati-grey text-white">
         <div class="mx-5 py-2 columns-3">
           <a href="https://iatistandard.org" title="IATI Standard Website"><IatiLogo colour="#fff" class="h-12" /></a>
-          <h1 class="text-3xl pt-5">
+          <h1 class="text-3xl mt-5">
             <a href="/"><b>Datastore Search</b></a><span style="color:green"> beta </span>
           </h1>
           <nav aria-label="Navigation" class="pt-6 ml-10">
