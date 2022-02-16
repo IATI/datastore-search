d07f434bbb90c4720a1fd5de685948043517b19f
akmiller01 <alex.k.miller@gmail.com>
2022-02-16 12:37:02 -0500

    Reconcile searchbar sizes with landing view

--- src/views/Simple.vue
+++ src/views/Simple.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex flex-col h-full min-w-fit sm:min-w-0">
-    <div class="sm:grid sm:grid-cols-10 lg:grid-cols-11 sm:gap-4 text-left border-b">
-      <div class="col-span-1 hidden lg:block"></div>
-      <div class="m-auto col-span-4 lg:col-span-3 pl-2 lg:pl-0">
+    <div class="md:grid md:grid-cols-10 lg:grid-cols-11 md:gap-4 text-left border-b">
+      <div class="col-span-1 hidden xl:block"></div>
+      <div class="m-auto col-span-4 xl:col-span-3 pl-2 lg:pl-0">
         <SearchBar />
       </div>
       <div v-if="global.state.responseTotal > 0" class="flex col-span-3 justify-center items-center">
