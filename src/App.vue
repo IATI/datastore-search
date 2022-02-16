ad1acb9d5df3895390c56a5dcb35084675138d4b
akmiller01 <alex.k.miller@gmail.com>
2022-02-16 11:24:35 -0500

    From flex to inline-block for centering header

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,22 @@
   <div class="flex flex-col h-screen min-w-fit sm:min-w-0">
     <header aria-label="Main header">
       <div class="bg-iati-grey text-white">
-        <div class="mx-5 py-2 columns-1 sm:columns-2 md:columns-3">
+        <div class="mx-5 py-2">
+          <div class="inline-block h-12 my-2 align-middle w-full sm:w-1/2 md:w-1/3">
           <a href="https://iatistandard.org" title="IATI Standard Website"><IatiLogo colour="#fff" class="h-12 mx-auto" /></a>
-          <h1 class="text-3xl pt-7">
-            <a href="/"><b>Datastore Search</b></a><span style="color:green"> beta </span>
-          </h1>
-          <nav aria-label="Navigation" class="pt-6 ml-10 hidden md:block">
-            <router-link v-if="$route.path === '/advanced' && !global.state.responseDocs" to="/" class="hover:underline">Switch to Simple Search</router-link>
-            <router-link v-if="$route.path === '/advanced' && global.state.responseDocs" to="/simple" class="hover:underline">Switch to Simple Search</router-link>
-            <router-link v-if="$route.path !== '/advanced'" to="/advanced" class="hover:underline">Switch to Advanced Search</router-link>
-          </nav>
+          </div>
+          <div class="inline-block h-full my-2 align-middle w-full sm:w-1/2 md:w-1/3">
+            <h1 class="text-3xl">
+              <a href="/"><b>Datastore Search</b><span style="color:green"> beta </span></a>
+            </h1>
+          </div>
+          <div class="hidden md:inline-block h-full my-2 align-middle w-full sm:w-1/2 md:w-1/3">
+            <nav aria-label="Navigation" class="ml-10">
+              <router-link v-if="$route.path === '/advanced' && !global.state.responseDocs" to="/" class="hover:underline">Switch to Simple Search</router-link>
+              <router-link v-if="$route.path === '/advanced' && global.state.responseDocs" to="/simple" class="hover:underline">Switch to Simple Search</router-link>
+              <router-link v-if="$route.path !== '/advanced'" to="/advanced" class="hover:underline">Switch to Advanced Search</router-link>
+            </nav>
+          </div>
         </div>
       </div>
     </header>
