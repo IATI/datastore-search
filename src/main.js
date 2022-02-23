d981aa13c25c0c835ef2b7f18ab61286c422d84a
akmiller01 <alex.k.miller@gmail.com>
2022-02-23 08:10:57 -0500

    GA4 to test UA

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './index.css'
 
 createApp(App).use(VueGtag, {
     config: {
-      id: "G-YYYXLYKJML", // Placeholder, TODO: Replace with UA when live
+      id: "UA-110230511-10", // Placeholder, TODO: Replace with UA when live
       params: {
         anonymize_ip: true
       }
