925960648cc26ae1e3f8fe5651c4ab7e24ff7958
akmiller01 <alex.k.miller@gmail.com>
2022-03-07 11:57:29 -0500

    Simpler performance now

--- src/App.vue
+++ src/App.vue
@@ -121,13 +121,11 @@ import { time } from "vue-gtag";
 export default {
   provide: { global },
   mounted() {
-    const perfEntries = performance.getEntriesByType("navigation");
-    perfEntries.forEach((entry) => {
-      time({
-        name: "load",
-        value: entry.duration,
-        event_category: "Initial application load",
-      });
+    const timeSincePageLoad = Math.round(performance.now());
+    time({
+      name: "load",
+      value: timeSincePageLoad,
+      event_category: "Initial application load",
     });
   },
 };
