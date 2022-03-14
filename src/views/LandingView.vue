ad0ce04b8ac9ea2b08579caf260f57b716d465aa
Alex Lydiate <alexl@devinit.org>
2022-03-14 10:00:51 +0000

    Hide advanced button on mobile

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -4,7 +4,7 @@
       <SearchBar />
       <router-link
         to="/advanced"
-        class="bg-slate-100 hover:bg-slate-300 text-slate-600 py-2 px-4 rounded"
+        class="bg-slate-100 hover:bg-slate-300 text-slate-600 py-2 px-4 rounded hide-on-mobile"
         >Switch to Advanced Search</router-link
       >
     </div>
