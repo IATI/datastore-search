5af18b92d1f806f4bd87d5cd8df62f54eb342ff5
akmiller01 <alex.k.miller@gmail.com>
2022-03-16 13:52:18 -0400

    Allow word wrap on slash for dates with small width

--- src/components/ResultItem.vue
+++ src/components/ResultItem.vue
@@ -4,11 +4,21 @@
     <div class="col-span-3 meta">
       Publisher: <b>{{ doc.reporting_org_narrative[0] }}</b>
     </div>
-    <div class="col-span-3 meta">IATI Identifer: <b>{{ doc.iati_identifier }}</b></div>
-    <div v-if="doc.actualStart" class="col-span-1 meta">Start Date: <b>{{ prettyDate(doc.actualStart) }}</b></div>
-    <div v-if="! doc.actualStart && doc.plannedStart" class="col-span-1 meta">Planned Start Date: <b>{{ prettyDate(doc.plannedStart) }}</b></div>
-    <div v-if="doc.actualEnd" class="col-span-1 meta">End Date: <b>{{ prettyDate(doc.actualEnd) }}</b></div>
-    <div v-if="!doc.actualEnd && doc.plannedEnd" class="col-span-1 meta">Planned End Date: <b>{{ prettyDate(doc.plannedEnd) }}</b></div>
+    <div class="col-span-3 meta">
+      IATI Identifer: <b>{{ doc.iati_identifier }}</b>
+    </div>
+    <div v-if="doc.actualStart" class="col-span-1 meta">
+      Start Date: <b v-html="prettyDate(doc.actualStart)"></b>
+    </div>
+    <div v-if="!doc.actualStart && doc.plannedStart" class="col-span-1 meta">
+      Planned Start Date: <b v-html="prettyDate(doc.plannedStart)"></b>
+    </div>
+    <div v-if="doc.actualEnd" class="col-span-1 meta">
+      End Date: <b v-html="prettyDate(doc.actualEnd)"></b>
+    </div>
+    <div v-if="!doc.actualEnd && doc.plannedEnd" class="col-span-1 meta">
+      Planned End Date: <b v-html="prettyDate(doc.plannedEnd)"></b>
+    </div>
     <div class="col-span-1"></div>
     <div class="col-span-1"></div>
     <div class="col-span-8 hover:underline text-sky-700">
@@ -34,21 +44,22 @@
 export default {
   name: "ResultItem",
   inject: ["global"],
-  methods: {
-    prettyDate: function (dt) {
-      return new Date(dt).toLocaleDateString("en-gb", {
-        year: "numeric",
-        month: "numeric",
-        day: "numeric",
-      });
-    },
-  },
   props: {
     doc: {
       type: Object,
       default() {
         return {};
       },
+    },
+  },
+  methods: {
+    prettyDate: function (dt) {
+      const dateString = new Date(dt).toLocaleDateString("en-gb", {
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+      });
+      return dateString.replaceAll("/", "/<wbr/>");
     },
   },
 };
