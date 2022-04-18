de714382a68f3b0270751efc091eae5b5bca168a
akmiller01 <alex.k.miller@gmail.com>
2022-04-18 09:57:25 -0400

    Clear search results when clicking back to simple

--- src/global.js
+++ src/global.js
@@ -689,6 +689,16 @@ const sortResults = async (field) => {
   }
 };
 
+const resetResults = () => {
+  (state.query = null),
+    (state.responseDocs = null),
+    (state.responseTotal = null),
+    (state.responseStart = null),
+    (state.numberPages = null),
+    (state.simpleSearch = null),
+    (state.simpleSearchTerm = null);
+};
+
 // Helper functions, not exported:
 
 const cleanSolrQueryString = (qString) => {
@@ -782,4 +792,5 @@ export default {
   sortFields,
   sortResults,
   importSimpleSearchToAdv,
+  resetResults,
 };
