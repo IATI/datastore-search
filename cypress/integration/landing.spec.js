f13bfa6ce842e77b97fa8a46d5eeb2d357431c2f
nosvalds <niko@devinit.org>
2023-01-09 09:17:13 +0000

    formatting

--- cypress/integration/landing.spec.js
+++ cypress/integration/landing.spec.js
@@ -1,30 +1,30 @@
 /// <reference types="Cypress"/>
 
-describe("The landing view", () => {
-  it("has a header", () => {
-    cy.visit("/");
-    cy.contains("Datastore Search");
-  });
-
-  it("has a visible search bar", () => {
-    cy.visit("/");
-    cy.get("input[title=Search]").should("be.visible");
-  });
-
-  it("has a footer", () => {
-    cy.visit("/");
-    cy.contains("IATI Unified Platform");
-  });
-
-  describe("search bar", () => {
-    it("routes a search to the simple view", () => {
-      cy.get("input[title=Search]").type("test");
-      cy.get("button[aria-label=Submit]").click();
-      cy.url().should("includes", "/simple");
+describe('The landing view', () => {
+    it('has a header', () => {
+        cy.visit('/');
+        cy.contains('Datastore Search');
     });
 
-    it("retains the search term after routing", () => {
-      cy.get("input[title=Search]").should("have.value", "test");
+    it('has a visible search bar', () => {
+        cy.visit('/');
+        cy.get('input[title=Search]').should('be.visible');
     });
-  });
+
+    it('has a footer', () => {
+        cy.visit('/');
+        cy.contains('IATI Unified Platform');
+    });
+
+    describe('search bar', () => {
+        it('routes a search to the simple view', () => {
+            cy.get('input[title=Search]').type('test');
+            cy.get('button[aria-label=Submit]').click();
+            cy.url().should('includes', '/simple');
+        });
+
+        it('retains the search term after routing', () => {
+            cy.get('input[title=Search]').should('have.value', 'test');
+        });
+    });
 });
