9c998c8056ae5a338e92ddc22ddbc674de1877fb
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-02-15 16:40:07 +0300

    Add size prop to AppButton

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,6 +1,7 @@
 <script setup>
 const props = defineProps({
     variant: { type: String, default: 'yellow' },
+    size: { type: String, default: 'base' },
     ariaLabel: { type: String, default: '' },
 });
 const emits = defineEmits(['click']);
@@ -9,16 +10,17 @@ const classes = {
     'bg-btn-green': props.variant === 'green',
     'bg-btn-red': props.variant === 'red',
     'bg-btn-yellow': props.variant === 'yellow',
+    'text-xs h-8': props.size === 'sm',
 };
 </script>
 
 <template>
     <button
         :aria-label="props.ariaLabel"
-        class="hover:bg-iati-grey text-white py-1.5 px-5 rounded inline-flex"
+        class="hover:bg-iati-grey text-white py-1.5 px-5 rounded"
         :class="classes"
         @click="emits('click')"
     >
-        <slot />
+        <span class="p-[2px] inline-flex align-middle"><slot /></span>
     </button>
 </template>
