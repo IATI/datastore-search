de49a2a44902b0af964fc3c9fb8a007af9359176
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-02-15 21:29:35 +0300

    Add group items via add rule button

--- src/designs/FilterGroup.vue
+++ src/designs/FilterGroup.vue
@@ -1,20 +1,30 @@
 <script setup>
+import { ref } from 'vue';
 import FilterGroupActions from './FilterGroupActions.vue';
 import FilterGroupItem from './FilterGroupItem.vue';
 
 const props = defineProps({ filter: { type: Object, default: () => {} } });
 
+const ruleCount = ref(0);
+const groupCount = ref(0);
+
 const onAddRule = () => {
-    console.log('Adding Rule');
+    ruleCount.value++;
 };
 const onAddGroup = () => {
-    console.log('Adding Group');
+    groupCount.value++;
 };
 </script>
 
 <template>
     <div class="p-2 border">
         <FilterGroupActions @add-rule="onAddRule" @add-group="onAddGroup" />
-        <FilterGroupItem :filter="props.filter" />
+        <div v-if="ruleCount">
+            <FilterGroupItem
+                v-for="index in [...Array(ruleCount).keys()]"
+                :key="index"
+                :filter="props.filter"
+            />
+        </div>
     </div>
 </template>
