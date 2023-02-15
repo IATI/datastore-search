0619a8f02f484bffd17afc4db2e6a70f8d456787
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-02-15 16:44:50 +0300

    Add rule and add group buttons

--- src/designs/FilterGroup.vue
+++ src/designs/FilterGroup.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import AppButton from '../components/AppButton.vue';
 
 const operator = ref('AND');
 const toggleOperator = () => {
@@ -11,21 +12,29 @@ const buttonClasses =
 
 <template>
     <div class="inline-flex" role="toolbar">
-        <button
-            :class="[operator === 'AND' ? 'bg-blue-300' : '', buttonClasses]"
-            class="border-l border-t border-b rounded-l"
-            type="button"
-            @click="toggleOperator"
-        >
-            {{ $t('message.and') }}
-        </button>
-        <button
-            :class="[operator === 'OR' ? 'bg-blue-300' : '', buttonClasses]"
-            type="button"
-            class="border-r border-t border-b rounded-r"
-            @click="toggleOperator"
-        >
-            {{ $t('message.or') }}
-        </button>
+        <div class="mr-5">
+            <button
+                :class="[operator === 'AND' ? 'bg-blue-300' : '', buttonClasses]"
+                class="border-l border-t border-b rounded-l"
+                type="button"
+                @click="toggleOperator"
+            >
+                {{ $t('message.and') }}
+            </button>
+            <button
+                :class="[operator === 'OR' ? 'bg-blue-300' : '', buttonClasses]"
+                type="button"
+                class="border-r border-t border-b rounded-r"
+                @click="toggleOperator"
+            >
+                {{ $t('message.or') }}
+            </button>
+        </div>
+        <AppButton size="sm" class="mr-2">
+            <span class="uppercase">Add Rule</span>
+        </AppButton>
+        <AppButton size="sm" variant="red">
+            <span class="uppercase">Add Group</span>
+        </AppButton>
     </div>
 </template>
