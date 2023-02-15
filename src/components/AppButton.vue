c0918e68da9fa4ebe5d42c86b3e65d10787db845
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-02-15 14:09:32 +0300

    Extract AppButton

---
+++ src/components/AppButton.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    variant: { type: String, default: 'yellow' },
+    ariaLabel: { type: String, default: '' },
+});
+const emits = defineEmits(['click']);
+
+const classes = {
+    'bg-btn-green': props.variant === 'green',
+    'bg-btn-red': props.variant === 'red',
+    'bg-btn-yellow': props.variant === 'yellow',
+};
+</script>
+
+<template>
+    <button
+        :aria-label="props.ariaLabel"
+        class="hover:bg-iati-grey text-white py-1.5 px-5 rounded inline-flex"
+        :class="classes"
+        @click="emits('click')"
+    >
+        <slot />
+    </button>
+</template>
--- src/designs/SideBarButtons.vue
+++ src/designs/SideBarButtons.vue
@@ -4,45 +4,49 @@ import { PlusCircleIcon } from '@heroicons/vue/20/solid';
 import { ArrowDownIcon } from '@heroicons/vue/20/solid';
 import { ArrowUpIcon } from '@heroicons/vue/20/solid';
 import { PlayIcon } from '@heroicons/vue/20/solid';
+import AppButton from '../components/AppButton.vue';
 
 const global = inject('global');
 </script>
 
 <template>
     <div id="buttons">
-        <div v-if="global.state.filters.length > 0" class="border-solid border-t border-b py-5">
-            <button
-                :aria-label="$t('message.run_aria')"
-                class="bg-btn-green hover:bg-iati-grey text-white py-1.5 px-5 rounded float-right ml-5 mr-8 w-2/24 inline-flex"
-                @click="global.run()"
-            >
-                <PlayIcon class="h-4 w-5 text-grey-300 mr-1 relative top-[3px]" />
-                <span>{{ $t('message.run') }}</span>
-            </button>
-            <button
-                :aria-label="$t('message.export_aria')"
-                class="bg-btn-red hover:bg-iati-grey text-white py-1.5 px-5 rounded ml-5 w-3/24 inline-flex"
-                @click="global.toggleExportModal()"
-            >
-                <ArrowDownIcon class="h-4 w-5 text-grey-300 mr-1 float-left relative top-[3px]" />
-                <span class="float-left">{{ $t('message.export') }}</span>
-            </button>
-            <button
-                :aria-label="$t('message.import_aria')"
-                class="bg-btn-red hover:bg-iati-grey text-white py-1.5 px-5 rounded ml-5 w-3/24 inline-flex"
-                @click="global.toggleImportModal()"
-            >
-                <ArrowUpIcon class="h-4 w-5 text-grey-300 mr-1 float-left relative top-[3px]" />
-                <span class="float-left">{{ $t('message.import') }}</span>
-            </button>
-            <button
+        <div
+            v-if="global.state.filters.length > 0"
+            class="border-solid border-t border-b py-5 flex justify-between mx-3"
+        >
+            <AppButton
                 :aria-label="$t('message.add_aria')"
-                class="bg-btn-yellow hover:bg-iati-grey text-white py-1.5 px-5 rounded float-left ml-8 w-2/24 inline-flex"
+                variant="yellow"
                 @click="global.addFilter()"
             >
-                <PlusCircleIcon class="h-4 w-5 text-grey-300 mr-1 float-left relative top-[3px]" />
-                <span class="float-left">{{ $t('message.add') }}</span>
-            </button>
+                <PlusCircleIcon class="h-4 w-5 text-grey-300 mr-1 relative top-[3px]" />
+                <span>{{ $t('message.add') }}</span>
+            </AppButton>
+            <div>
+                <AppButton
+                    :aria-label="$t('message.export_aria')"
+                    variant="red"
+                    class="mr-1"
+                    @click="global.toggleExportModal()"
+                >
+                    <ArrowDownIcon class="h-4 w-5 text-grey-300 mr-1 relative top-[3px]" />
+                    <span>{{ $t('message.export') }}</span>
+                </AppButton>
+                <AppButton
+                    :aria-label="$t('message.import_aria')"
+                    variant="red"
+                    class="ml-1"
+                    @click="global.toggleImportModal()"
+                >
+                    <ArrowUpIcon class="h-4 w-5 text-grey-300 mr-1 relative top-[3px]" />
+                    <span>{{ $t('message.import') }}</span>
+                </AppButton>
+            </div>
+            <AppButton :aria-label="$t('message.run_aria')" variant="green" @click="global.run()">
+                <PlayIcon class="h-4 w-5 text-grey-300 mr-1 relative top-[3px]" />
+                <span>{{ $t('message.run') }}</span>
+            </AppButton>
         </div>
         <div v-if="global.state.filters.length === 0">
             <button
