63e556a25fd561dbe8e8d3101ce9588420034024
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-16 17:20:59 +0300

    Reduce the size of the FilterGroup buttons

--- src/designs/FilterGroupActions.vue
+++ src/designs/FilterGroupActions.vue
@@ -12,12 +12,12 @@ const toggleOperator = () => {
     emits('toggleOperator', operator.value);
 };
 const buttonClasses =
-    'h-8 text-xs px-5 py-1 text-gray-700 font-medium text-xs leading-tight uppercase hover:bg-blue-500 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out';
+    'h-8 text-xs px-2 py-1 text-gray-700 font-medium text-xs leading-tight uppercase hover:bg-blue-500 focus:outline-none focus:ring-0 active:bg-blue-800 transition duration-150 ease-in-out';
 </script>
 
 <template>
     <div class="inline-flex items-center w-full" role="toolbar">
-        <div class="mr-5">
+        <div class="mr-3">
             <button
                 :class="[operator === 'AND' ? 'bg-blue-300' : '', buttonClasses]"
                 class="border-l border-t border-b rounded-l"
@@ -35,10 +35,10 @@ const buttonClasses =
                 {{ $t('message.or') }}
             </button>
         </div>
-        <AppButton variant="outline" size="sm" class="mr-2" @click="emits('addRule')">
+        <AppButton variant="outline" size="sm" class="mr-2 px-[8px]" @click="emits('addRule')">
             <span class="uppercase">Add Rule</span>
         </AppButton>
-        <AppButton variant="outline-accent" size="sm" @click="emits('addGroup')">
+        <AppButton variant="outline-accent" class="px-[8px]" size="sm" @click="emits('addGroup')">
             <span class="uppercase">Add Group</span>
         </AppButton>
         <div v-if="deletable" class="p-2 float-right">
