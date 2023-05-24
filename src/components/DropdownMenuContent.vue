ef1e920f3d87c14d108d1280e35ad3d3991715b7
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-24 17:33:25 +0300

    Add DropdownMenu

---
+++ src/components/DropdownMenuContent.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { inject } from 'vue';
+
+const { active } = inject('sharedState');
+</script>
+
+<template>
+    <div v-if="active" class="menu">
+        <slot />
+    </div>
+</template>
+
+<style>
+.menu {
+    cursor: auto;
+    position: absolute;
+    outline: 0;
+    /* top: 100%; */
+    min-width: -webkit-max-content;
+    min-width: -moz-max-content;
+    min-width: max-content;
+    margin: 3px 0;
+    padding: 0 0;
+    background: #fff;
+    font-size: 1em;
+    text-shadow: none;
+    text-align: left;
+    box-shadow: 0 2px 3px 0 rgba(34, 36, 38, 0.15);
+    border: 1px solid rgba(34, 36, 38, 0.15);
+    border-radius: 0.28571429rem;
+    transition: opacity 0.1s ease;
+    z-index: 11;
+    will-change: transform, opacity;
+}
+</style>
