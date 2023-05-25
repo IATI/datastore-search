d9d19561f7259abd7a506f158571ccf9b0367e6f
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-25 23:29:10 +0300

    Extract AppFooter

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import AppHeader from './components/AppHeader.vue';
-import LinkedInLogo from './components/LinkedInLogo.vue';
-import TwitterLogo from './components/TwitterLogo.vue';
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
@@ -10,58 +9,7 @@ import TwitterLogo from './components/TwitterLogo.vue';
         <div class="flex flex-col h-screen min-w-fit sm:min-w-0">
             <div class="flex-grow"><router-view /></div>
         </div>
-        <footer class="bg-iati-grey text-white text-left text-sm">
-            <div class="mx-auto">
-                <div class="py-4 px-12 xl:max-w-[75%] mx-auto">
-                    <p class="inline-block float-left mt-1">
-                        {{ $t('message.datastore_search_is_part_of_the_iati_unified_platform') }}
-                    </p>
-
-                    <div class="float-right">
-                        <p class="inline-block float-left mt-1 mr-5">
-                            <a
-                                class="hover:underline"
-                                role="link"
-                                :href="
-                                    'https://iatistandard.org/' +
-                                    global.state.language +
-                                    '/privacy-policy/'
-                                "
-                                >{{ $t('message.privacy') }}</a
-                            >
-                        </p>
-                        <p class="inline-block float-left mt-1 mr-5">
-                            <a
-                                class="hover:underline"
-                                role="link"
-                                :href="
-                                    'https://iatistandard.org/' +
-                                    global.state.language +
-                                    '/contact/'
-                                "
-                                >{{ $t('message.contact') }}</a
-                            >
-                        </p>
-                        <a
-                            class="inline-block ml-2"
-                            role="link"
-                            :aria-label="$t('message.twitter_aria')"
-                            href="https://twitter.com/IATI_aid"
-                        >
-                            <TwitterLogo class="w-8 h-8" />
-                        </a>
-                        <a
-                            class="inline-block ml-5"
-                            role="link"
-                            :aria-label="$t('message.linkedin_aria')"
-                            href="https://www.linkedin.com/company/international-aid-transparency-initiative/"
-                        >
-                            <LinkedInLogo class="w-8 h-8" />
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <AppFooter />
     </div>
 </template>
 
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { inject } from 'vue';
+import LinkedInLogo from './LinkedInLogo.vue';
+import TwitterLogo from './TwitterLogo.vue';
+
+const global = inject('global');
+</script>
+
+<template>
+    <footer class="bg-iati-grey text-white text-left text-sm">
+        <div class="mx-auto">
+            <div class="py-4 px-12 xl:max-w-[75%] mx-auto flex justify-between align-middle">
+                <p class="inline-block float-left mt-1">
+                    {{ $t('message.datastore_search_is_part_of_the_iati_unified_platform') }}
+                </p>
+
+                <div class="flex align-middle">
+                    <p class="inline-block float-left mt-1 mr-5">
+                        <a
+                            class="hover:underline"
+                            role="link"
+                            :href="
+                                'https://iatistandard.org/' +
+                                global.state.language +
+                                '/privacy-policy/'
+                            "
+                            >{{ $t('message.privacy') }}</a
+                        >
+                    </p>
+                    <p class="inline-block float-left mt-1 mr-5">
+                        <a
+                            class="hover:underline"
+                            role="link"
+                            :href="
+                                'https://iatistandard.org/' + global.state.language + '/contact/'
+                            "
+                            >{{ $t('message.contact') }}</a
+                        >
+                    </p>
+                    <a
+                        class="inline-block ml-2"
+                        role="link"
+                        :aria-label="$t('message.twitter_aria')"
+                        href="https://twitter.com/IATI_aid"
+                    >
+                        <TwitterLogo class="w-8 h-8" />
+                    </a>
+                    <a
+                        class="inline-block ml-5"
+                        role="link"
+                        :aria-label="$t('message.linkedin_aria')"
+                        href="https://www.linkedin.com/company/international-aid-transparency-initiative/"
+                    >
+                        <LinkedInLogo class="w-8 h-8" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
