c16d4a23e2ce8c2afecbe10de09ad67eb1de9762
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-26 00:12:10 +0300

    Update SimpleView styling

--- src/views/SimpleView.vue
+++ src/views/SimpleView.vue
@@ -4,6 +4,7 @@ import SearchBar from '../components/SearchBar.vue';
 import ResultList from '../components/ResultList.vue';
 import DownloadButtons from '../components/DownloadButtons.vue';
 import SortButtons from '../components/SortButtons.vue';
+import { formatNumber } from '../utils';
 
 const global = inject('global');
 const showAdvancedSearch = inject('showAdvancedSearch');
@@ -20,46 +21,54 @@ const goToAdvanced = () => {
 
 <template>
     <div class="flex flex-col h-full min-w-fit sm:min-w-0">
-        <div class="px-2 md:grid md:grid-cols-11 lg:grid-cols-12 md:gap-4 text-left border-b">
-            <div class="col-span-1 hidden 2xl:block"></div>
-            <div class="my-auto col-span-3 md:col-span-4 lg:col-span-3 2xl:col-span-2 ml-2">
-                <SearchBar class="simple" />
+        <div class="px-2 md:gap-4 text-left border-b">
+            <div class="inline-flex">
+                <div class="my-auto mr-2">
+                    <SearchBar class="simple" />
+                </div>
+                <button
+                    v-if="global.state.responseTotal > 0"
+                    class="py-2 px-4 justify-self-start 2xl:ml-6 my-auto bg-slate-100 hover:bg-slate-300 text-slate-600 font-medium rounded"
+                    @click="goToAdvanced"
+                >
+                    {{ $t('message.advanced') }}
+                </button>
             </div>
-            <button
-                v-if="global.state.responseTotal > 0"
-                class="hidden lg:block col-span-2 xl:col-span-1 p-2 justify-self-start 2xl:ml-6 my-auto bg-iati-blue hover:bg-iati-grey text-white font-bold rounded"
-                @click="goToAdvanced"
-            >
-                {{ $t('message.advanced') }}
-            </button>
-            <div
-                v-if="global.state.responseTotal > 0"
-                class="flex col-span-1 xl:col-span-2 justify-center items-center"
-            >
-                <!-- eslint-disable vue/no-v-html -->
-                <span
-                    class="my-2"
-                    v-html="
-                        $t('message.found_activities', {
-                            count: global.state.responseTotal,
-                        })
-                    "
-                ></span>
-                <!-- eslint-enable vue/no-v-html -->
-            </div>
-            <div
-                v-if="global.state.responseTotal > 0"
-                class="flex col-span-2 md:col-span-3 m-1 justify-left items-center"
-            >
-                <SortButtons
-                    v-if="global.state.responseTotal != null && global.state.responseTotal > 0"
-                />
-            </div>
-            <div
-                v-if="global.state.responseTotal > 0"
-                class="flex col-span-3 justify-left items-center m-1"
-            >
-                <DownloadButtons />
+            <div class="flex items-center mb-3 justify-between">
+                <div
+                    v-if="global.state.responseTotal > 0"
+                    class="flex justify-center items-center mr-5"
+                >
+                    <!-- eslint-disable vue/no-v-html -->
+                    <span
+                        class="my-2"
+                        v-html="
+                            $t('message.found_activities', {
+                                count: formatNumber(global.state.responseTotal),
+                            })
+                        "
+                    ></span>
+                    <!-- eslint-enable vue/no-v-html -->
+                </div>
+                <div class="inline-flex">
+                    <div
+                        v-if="global.state.responseTotal > 0"
+                        class="flex mx-1 justify-left items-center"
+                    >
+                        <SortButtons
+                            v-if="
+                                global.state.responseTotal != null && global.state.responseTotal > 0
+                            "
+                            class="max-w-full mb-0"
+                        />
+                    </div>
+                    <div
+                        v-if="global.state.responseTotal > 0"
+                        class="flex justify-left items-center m-1"
+                    >
+                        <DownloadButtons />
+                    </div>
+                </div>
             </div>
         </div>
         <ResultList />
