697b409735f43ca05efe98e894e44711a110558d
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-26 13:18:26 +0300

    Add responsive styling for the app header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,14 +7,14 @@ import LanguageSelector from './LanguageSelector.vue';
 <template>
     <header :aria-label="$t('message.main_header')" class="bg-iati-grey">
         <div class="text-white ml-auto mr-auto max-w-[1200px] relative">
-            <div class="mx-5 flex">
-                <div class="inline-block my-2 w-full sm:w-1/2 md:w-1/3">
+            <div class="md:mx-5 md:flex">
+                <div class="flex justify-center md:inline-block my-2 w-full sm:w-1/2 md:w-1/3">
                     <a href="https://iatistandard.org" :title="$t('message.iati_standard_website')">
                         <IatiLogo colour="#fff" class="h-12" />
                     </a>
                 </div>
                 <div class="inline-block h-full my-2 align-middle w-full sm:w-1/2 md:w-1/3">
-                    <h1 class="text-3xl mt-4 pb-3">
+                    <h1 class="text-3xl mt-1 md:mt-4 pb-3">
                         <a href="/">
                             <b>{{ $t('message.datastore_search') }}</b>
                         </a>
@@ -22,9 +22,9 @@ import LanguageSelector from './LanguageSelector.vue';
                 </div>
 
                 <div
-                    class="hidden md:inline-flex justify-between h-full my-2 align-middle w-full sm:w-1/2 md:w-1/3"
+                    class="inline-flex items-center mx-2 md:mx-0 justify-around md:justify-between h-full my-2 align-middle w-full sm:w-1/2 md:w-1/3"
                 >
-                    <nav :aria-label="$t('message.navigation')" class="ml-10 mt-5 text-xl">
+                    <nav :aria-label="$t('message.navigation')" class="md:ml-10 md:mt-5 text-xl">
                         <router-link
                             v-if="$route.path === '/advanced'"
                             to="/"
--- src/components/LanguageSelector.vue
+++ src/components/LanguageSelector.vue
@@ -5,7 +5,7 @@ import { inject } from 'vue';
 const global = inject('global');
 </script>
 <template>
-    <div class="inline-block h-10 pt-3 mb-3">
+    <div class="inline-block md:h-10 pt-3 mb-3">
         <LanguageIcon class="h-7 w-7 text-grey-300 float-left mr-1 mt-1" />
         <select
             class="h-10 float-left bg-white border rounded w-auto py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
