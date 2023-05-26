c210e9b3d1496b5edd153869264b8199b2b4240e
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-26 13:23:50 +0300

    Show advanced button on mobile

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -36,7 +36,7 @@ watch(
         >
             <SearchBar class="landing" @search="onSearch" />
             <button
-                class="bg-slate-100 hover:bg-slate-300 text-slate-600 py-2 px-4 rounded hide-on-mobile"
+                class="bg-slate-100 hover:bg-slate-300 text-slate-600 py-2 px-4 rounded"
                 @click="showAdvancedSearch = true"
             >
                 {{ $t('message.switch_to_advanced_search') }}
