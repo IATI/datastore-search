3aac2e2e59b8139ebb91538a3d631d0a8feec115
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-29 18:47:25 +0300

    Update AppHeader responsive styling

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,13 +7,13 @@ import LanguageSelector from './LanguageSelector.vue';
 <template>
     <header :aria-label="$t('message.main_header')" class="bg-iati-grey">
         <div class="text-white ml-auto mr-auto max-w-[1200px] relative">
-            <div class="md:mx-5 md:flex">
-                <div class="flex justify-center md:inline-block my-2 w-full sm:w-1/2 md:w-1/3">
+            <div class="lg:mx-5 lg:flex">
+                <div class="flex justify-center lg:inline-block my-2 w-full lg:w-1/2 xl:w-1/3">
                     <a href="https://iatistandard.org" :title="$t('message.iati_standard_website')">
                         <IatiLogo colour="#fff" class="h-12" />
                     </a>
                 </div>
-                <div class="inline-block h-full my-2 align-middle w-full sm:w-1/2 md:w-1/3">
+                <div class="inline-block h-full my-2 align-middle w-full md:w-1/2 lg:w-1/3">
                     <h1 class="text-3xl mt-1 md:mt-4 pb-3">
                         <a href="/">
                             <b>{{ $t('message.datastore_search') }}</b>
@@ -22,9 +22,9 @@ import LanguageSelector from './LanguageSelector.vue';
                 </div>
 
                 <div
-                    class="inline-flex items-center mx-2 md:mx-0 justify-around md:justify-between h-full my-2 align-middle w-full sm:w-1/2 md:w-1/3"
+                    class="inline-flex items-center mx-2 lg:mx-0 justify-around lg:justify-between h-full my-2 align-middle w-full md:w-1/2 lg:w-1/3"
                 >
-                    <nav :aria-label="$t('message.navigation')" class="md:ml-10 md:mt-5 text-xl">
+                    <nav :aria-label="$t('message.navigation')" class="xl:ml-10 md:mt-5 text-xl">
                         <router-link
                             v-if="$route.path === '/advanced'"
                             to="/"
