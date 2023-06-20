c6d8c40f889fc586fb36470c1f4ac7a026f9ad44
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-06-20 17:30:54 +0300

    Format filter string operator

--- src/components/FilterStringItem.vue
+++ src/components/FilterStringItem.vue
@@ -4,6 +4,20 @@ defineProps({
     showOperator: { type: Boolean, default: true },
     operatorPosition: { type: String, default: 'start' }, // alternative is "end"
 });
+
+const formatOperator = (operator) => {
+    switch (operator.toLowerCase()) {
+        case 'notequals':
+            return 'not equals';
+        case 'greaterthan':
+            return 'greater than';
+        case 'lessthan':
+            return 'less than';
+
+        default:
+            return operator;
+    }
+};
 </script>
 
 <template>
@@ -16,7 +30,9 @@ defineProps({
             {{ filter.selectedOption.label }}
         </span>
         <span v-else class="text-iati-grey pr-2">{{ filter.field }}</span>
-        <span v-if="filter.type !== 'grouping'" class="italic pr-2">{{ filter.operator }}</span>
+        <span v-if="filter.type !== 'grouping'" class="italic pr-2">
+            {{ formatOperator(filter.operator) }}
+        </span>
         <span v-if="filter.type !== 'grouping'" class="text-iati-accent pr-2">
             {{ filter.value }}
         </span>
